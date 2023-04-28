86e99564bcee4ec4a3b9a5cb7bc6d3b10d42eda0
Akshay_112122 <89520508+akkhyyy-101@users.noreply.github.com>
2023-04-28 17:54:06 +0530

    Add files via upload

---
+++ Transport Simple/script.js
@@ -0,0 +1,14 @@
+
+const getStartedButton = document.querySelector('.button.get-started');
+const contactUsButton = document.querySelector('.button.contact-us');
+
+// Add click event listeners to the buttons
+getStartedButton.addEventListener('click', function () {
+
+    console.log('Get Started button clicked');
+});
+
+contactUsButton.addEventListener('click', function () {
+
+    console.log('Contact Us button clicked');
+});
